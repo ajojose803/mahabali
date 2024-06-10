6c467d3864c0c2abb97a14a1bd4b30b09212fc49
ajojose803 <ajojose803@gmail.com>
2024-06-10 10:20:54 +0530

    Added Method Override

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const path = require('path');
 require('dotenv').config();
 const session = require('express-session');
 const passport = require('passport');
+const flash = require('connect-flash'); 
+const nocache = require("nocache");
+require('./auth/auth');
+const mongoose = require('mongoose');
+const asyncHandler = require('express-async-handler');
+const methodOverride = require('method-override');
+
+
 const userRouter = require('./routes/userRoutes');
 const adminRouter = require('./routes/adminRoutes');
-const flash = require('connect-flash'); // Corrected 'flash' to 'connect-flash'
-require('./auth/auth');
-const app = express();
 
-const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 7000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI)
@@ -19,7 +25,6 @@ mongoose.connect(MONGODB_URI)
         console.error(error);
     });
 
-const PORT = process.env.PORT || 7000;
 
 app.use(
     session({
@@ -31,17 +36,19 @@ app.use(
         }
     })
 );
-
 app.use(flash());
-
-app.set('views', path.join(__dirname, 'views'));
-app.set("view engine", "ejs");
+app.use(nocache());
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(methodOverride('_method'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, '/public')));
