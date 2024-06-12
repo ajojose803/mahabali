388343bdeea4bf2394d06f149a36bbb077021e86
ajojose803 <ajojose803@gmail.com>
2024-06-12 08:45:25 +0530

    Updated consistent naming structure

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,39 +1,36 @@
-const express = require("express");
+const express = require('express');
 const adminRouter = express.Router();
-const adminController = require("../controller/adminController/adminController")
-const categoryController = require("../controller/adminController/categoryController")
-const productController = require("../controller/adminController/productController");
+const adminController = require('../controller/adminController/adminController');
+const categoryController = require('../controller/adminController/categoryController');
+const productController = require('../controller/adminController/productController');
 const userManagement = require('../controller/adminController/userManagement');
+const auth = require('../middleware/sessionAuth');
+const upload = require('../utils/multer')
+const nocache = require('nocache');
 
+adminRouter.use(nocache());
 
-adminRouter.route('/').get(adminController.adminPage)
-adminRouter.route('/login').get(adminController.adminPage)
+adminRouter.route('/').get(adminController.adminPage);
+adminRouter.route('/login').get(adminController.adminPage);
+adminRouter.route('/logout').get(adminController.adminLogout);
 
+adminRouter.route('/verifyLogin').post(adminController.adminLogin);
+adminRouter.route('/dashboard').get(auth.isAdAuth, adminController.adminDashboard);
 
-adminRouter.route('/verifyLogin').post(adminController.adminLogin)
-adminRouter.route('/dashboard').get(adminController.adminDashboard)
+// User Management System
+adminRouter.route('/users').get(auth.isAdAuth, userManagement.getUser);
+adminRouter.route('/users/change-status').get(userManagement.blockUser);
 
-
-//User Management System
-adminRouter.route('/users').get(userManagement.getUser)
-
-
-//category and new categories
-adminRouter.route('/category').get(categoryController.loadCategory)
+// Category and new categories
+adminRouter.route('/category').get(auth.isAdAuth, categoryController.loadCategory);
 adminRouter.route('/category/update-status').get(categoryController.listingStatusCategory);
 adminRouter.route('/add-category').post(categoryController.addNewCategory);
 adminRouter.route('/update-category').post(categoryController.updateCategory);
 
-//Changing the listing of the Categories
-//adminRouter.route('/categoryisListed/:id').get(categoryController.listingStatusCategory)
+// Products
+adminRouter.route('/products').get(auth.isAdAuth,productController.loadProduct);
+adminRouter.route('/add-product').get(productController.loadAddProduct);
+adminRouter.route('/add-product').post(upload.array ('image', 5),productController.addNewProduct);
+adminRouter.route('/edit-product/:id').post(upload.array ('image', 5),productController.editProduct);
 
-
-//products
-adminRouter.route('/products').get(productController.loadProduct)
-adminRouter.route('/add-product').get(productController.loadAddProduct)
-
-
-
-
-
-module.exports = adminRouter
+module.exports = adminRouter;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 require('../auth/auth')
 const userController = require ("../controller/UserController/userController")
-
-
+const productController = require ("../controller/UserController/userProductController")
+const cartController = require ("../controller/UserController/cartController")
+const auth = require("../middleware/sessionAuth")
+const nocache = require('nocache');
+const userProfile = require("../controller/UserController/userProfile")
+const checkout = require("../controller/UserController/checkoutController")
 
 const userRouter = express.Router();
 
-
+userRouter.use(nocache());
 
 userRouter.route('/').get(userController.loadHome)
 
@@ -27,7 +31,7 @@ userRouter.route('/login')
 
 
 userRouter.route('/login').post(userController.loginUser)
-userRouter.route('/forgot-passowrd').post(userController.forgotPassword);
+userRouter.route('/forgot-password').post(userController.forgotPassword);
 
 
 //auth
@@ -36,7 +40,42 @@ userRouter.route('/auth/google/callback').get(userController.googleCallback);
 userRouter.route('/auth/google/failure').get(userController.authFailure);
 
 
-//otp
-userRouter.route('/sendOtp',)
+//
+// userRouter.get('/account', (req,res)=> {
+// res.render('user/myAccount')
+// })
+
+
+//product
+userRouter.route('/products').get(auth.userAuth,productController.getAllProducts);
+userRouter.route('/product/:productId').get(auth.userAuth,productController.getProduct);
+
+//cart
+userRouter.route('/cart').get(auth.userAuth,cartController.LoadCart);
+userRouter.route('/add-to-cart/:productId').post(auth.userAuth,cartController.addtocart);
+userRouter.route('/cart/delete/:productId').get(auth.userAuth,cartController.deleteCart);
+userRouter.route('/cart/update').post(auth.userAuth,cartController.updateCart);
+userRouter.route('/cart/update-cart/:id').put(auth.userAuth,cartController.updateQuantity);
+// router.post('/cart/update-cart/:id', 
+
+//checkout
+userRouter.route('/checkout').get(auth.userAuth,checkout.loadCheckout);
+
+//profile.route()
+userRouter.route('/profile').get(auth.userAuth,userProfile.LoadProfile)
+
+//address
+userRouter.route('/profile/addAddress').get(auth.userAuth,userProfile.LoadAddAddress)
+userRouter.route('profile/address/new').post(auth.userAuth, userProfile.addaddress)
+userRouter.route('/profile/address').get(auth.userAuth,userProfile.showaddress)
+userRouter.route('/profile/editAddress/:id').get(auth.userAuth,userProfile.LoadEditAddress)
+userRouter.route('/addressupdated/:id').post(auth.userAuth,userProfile.editaddress)
+userRouter.route('/deleteAddress/:id').get(auth.userAuth,userProfile.deleteAddress)
+
+
+
+//Password
+userRouter.route('/reset-password').get(auth.userAuth,userProfile.LoadResetPassword)
+userRouter.route('/password-update').post(auth.userAuth,userProfile.updatePassword)
 
 module.exports = userRouter;
